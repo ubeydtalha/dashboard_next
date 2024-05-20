1d7c749c1b4b5c72da1c3defe5ac4514b30273c1
Ubeyd <ubeydtalhaalkan@gmail.com>
2024-05-20 17:03:48 +0200

    chore: Update createInvoice function to return a Promise<State> in action.ts and remove unused import in definitions.ts

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -34,7 +34,7 @@ export type State = {
     message?: string | null;
 };
 
-export async function createInvoice(prevState: State, formData: FormData) {
+export async function createInvoice(prevState: State, formData: FormData): Promise<State> {
 
 
     const validatedFields = CreateInvoice.safeParse({
